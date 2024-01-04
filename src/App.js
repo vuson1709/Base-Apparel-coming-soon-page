c6309891119680a838dc5cdcc81b66d62dafbe53
vuson1709 <thaison170996@gmail.com>
2024-01-03 16:17:30 -0800

    1st comit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+/* 
+We're coming soon Hello fellow shoppers! We're currently building our new fashion store. Add your email below to stay up-to-date with announcements and our launch deals. Email Address
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+Your users should be able to:
+
+View the optimal layout for the interface depending on their device's screen size
+See hover and focus states for all interactive elements on the page
+Receive an error message when the form is submitted if:
+The input field is empty
+The email address is not formatted correctly
+*/
+
+export default function ComingSoon() {
+  return <div>hello</div>;
 }
-
-export default App;
